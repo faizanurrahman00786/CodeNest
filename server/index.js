c206306fd900036b6c2056d053102983620d70a0
faizanurrahman00786 <160270586+faizanurrahman00786@users.noreply.github.com>
2025-01-06 22:24:46 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -20,7 +20,7 @@ app.use(fileUpload({
 const cors = require("cors");
 app.use(
 	cors({
-		origin: "https://ed-tech-2f5549rcp-faizanur-rahmans-projects.vercel.app",
+		origin: "https://ed-tech-beta-orcin.vercel.app",
 		credentials: true,
 	})
 )
