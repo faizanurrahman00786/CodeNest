651a7b6b9ad3b789777cdb48c33c49fd9299afff
faizanurrahman00786 <160270586+faizanurrahman00786@users.noreply.github.com>
2025-01-08 00:03:55 +0530

    Update Auth.js

--- server/controllers/Auth.js
+++ server/controllers/Auth.js
@@ -158,6 +158,7 @@ exports.login = async (req, res) => {
 			const options = {
 				expires: new Date(Date.now() + 3 * 24 * 60 * 60 * 1000),
 				httpOnly: true,
+				secure:true,
 				sameSite: 'None',
 			};
 			res.cookie("token", token, options).status(200).json({
